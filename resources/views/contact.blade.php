53f2faf9b5f6af0513c4d304014c80cbe53f6a3d
Jakhin <jack95.ly@googlemail.com>
2024-10-10 14:54:23 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <div class="container">
+        <h1>Welcome to contact Page</h1>
+        <!-- Your page content goes here -->
+    </div>
+</x-app-layout>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <div class="container">
+        <h1>Welcome to home Page</h1>
+        <!-- Your page content goes here -->
+    </div>
+</x-app-layout>
